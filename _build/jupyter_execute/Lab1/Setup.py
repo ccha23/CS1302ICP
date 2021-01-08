52706efaa6d90e697e8d948103abab3be802671d
Chung Chan <chung.chan@cityu.edu.hk>
2021-01-09 00:57:59 +0800

    ltjh

--- _build/jupyter_execute/Lab1/Setup.py
+++ _build/jupyter_execute/Lab1/Setup.py
@@ -7,11 +7,9 @@
 
 # ## JupyterHub
 
-# **What is JupyterHub?**
-
 # ### How to access the JupyterHub Server?
 
-# 1. Enter the url [ltjh.cs.cityu.edu.hk](https://ltjh.cs.cityu.edu.hk) in a web browser.
+# 1. Enter the url of the Jupyterhub server [ltjh.cs.cityu.edu.hk](https://ltjh.cs.cityu.edu.hk) in a web browser.
 # 1. Enter your [EID](https://www.cityu.edu.hk/esu/eid.htm) and Password in the fields `Username` and `Password` respectively.
 # 1. Click the `Sign In` button.
 
@@ -42,10 +40,8 @@
 # 1. Note that all the downloaded course materials will be placed under the `cs1302` folder of your home directory by default, so you need not go to the `Assignments` tab again to open the downloaded materials. 
 # E.g., you can access the `Setup.ipynb` notebook as follows:
 #     1. Going to the `File` tab, which is the default JupyterHub homepage after login or when you click the logo on the top left-hand corner.
-#     1. Enter the notebook URL https://ltjh.cs.cityu.edu.hk/user-redirect/tree/cs1302/Lab1/Setup.ipynb. (See the [documentation](https://jupyterhub.readthedocs.io/en/stable/reference/urls.html) for details.)
+#     1. Enter the notebook URL [ltjh.cs.cityu.edu.hk/user-redirect/tree/cs1302/Lab1/Setup.ipynb](https://ltjh.cs.cityu.edu.hk/user-redirect/tree/cs1302/Lab1/Setup.ipynb). (See the [documentation](https://jupyterhub.readthedocs.io/en/stable/reference/urls.html) for details.)
 # 1. If for any reason you want to Fetch `Lab1` again, you have to first rename your `Lab1` folder to a different name such as `Lab1_orig`. You can do so by selecting the folder and click rename. You can also remove the folder by evaluating `!rm -rf ~/cs1302/Lab1` in a code cell. (Be very cautious as removed folders cannot be recovered.)
-# 1. You may also play with the new interface called JupyterLab https://ltjh.cs.cityu.edu.hk/user-redirect/lab/.
-# However, the new interface does not support all the tools we need for lectures/labs. It is currently under [active development on GitHub](https://github.com/jupyterlab/jupyterlab), so be prepared to see [bugs](https://en.wikipedia.org/wiki/Software_bug).
 
 # ## Jupyter Notebook
 
@@ -55,41 +51,30 @@
 # 1. Click `Help->User Interface Tour` to learn the jupyter notebook interface. 
 # 1. Click `Help->Notebook Help` and skim through the tutorials on `Running Code` and `Working with Markdown Cells`.
 
-# Note that the tutorial page is static but you can copy to this notebook to run them. Let's run some code in this notebook.
-
-# **Exercise** Run the following cell to enable and configure the notebook extensions for this course.
-
-# In[ ]:
-
-
-get_ipython().run_cell_magic('javascript', '', 'IPython.notebook.config.update({\n  "load_extensions": {\n    "validate_assignment/main": true,\n    "execute_time/ExecuteTime": true,\n    "init_cell/main": true,\n    "printview/main": true,\n    "skip-traceback/main": true,\n    "hide_input/main": true,\n    "codefolding/main": true,\n    "scratchpad/main": true,\n    "spellchecker/main": true,\n    "create_assignment/main": true,\n    "ruler/main": true,\n    "collapsible_headings/main": true,\n    "latex_envs/latex_envs": true,\n    "exercise/main": true,\n    "select_keymap/main": true,\n    "toc2/main": true,\n    "code_prettify/code_prettify": true\n  },\n  "rise": {\n    "scroll": true\n  },\n  "toc": {\n    "skip_h1_title": true\n  }\n})')
-
-
-# - Refresh the page for the notebook extensions to take effect.
-# - You can learn more about the extensions from the [nbextensions page](/user-redirect/nbextensions).
-
-# Instead of evaluation a code cell, we often ask you to write a code in a particular cell. Make sure you fill in any place that says `YOUR CODE HERE` or "YOUR ANSWER HERE".
-
 # **Exercise** In learning a new computer language, the first program to write is often the ["Hello, World!"](https://en.wikipedia.org/wiki/%22Hello,_World!%22_program) program, which says Hello to the world. Type the program `print('Hello, World!')` below and run it with `Shift+Enter`.
 
-# In[ ]:
+# In[1]:
 
 
-# YOUR CODE HERE
-raise NotImplementedError()
+### BEGIN SOLUTION
+print('Hello, World!')
+### END SOLUTION
 
+
+# We often ask you to write a code in a particular cell. Make sure you fill in any place that says `YOUR CODE HERE` or "YOUR ANSWER HERE".
 
 # In order to check your work thoroughly, there will visible and hidden test cases. The following is a visible test you can run to check your answer: The test returns an assertion error only if your program does not print the correct message.
 
-# In[ ]:
+# In[2]:
 
 
 # Run this test cell right after running your "Hello, World!" program.
 import sys, io
 old_stdout, sys.stdout = sys.stdout, io.StringIO()
 exec(In[-2])
-assert sys.stdout.getvalue() == 'Hello, World!\n'
+printed = sys.stdout.getvalue()
 sys.stdout = old_stdout
+assert printed == 'Hello, World!\n'
 
 
 # **Tips**
@@ -97,12 +82,12 @@ sys.stdout = old_stdout
 # 1. To assess your solution thoroughly, we often run new tests hidden from you after you have submitted your notebook. There is no partial credit for a partially correct solution that works for the visible test but fails for the hidden test. Therefore, *you should ensure your solution works in general rather than just the visible tests*.
 # 1. You can click the `Validate` button to run all the visible tests.
 # 1. If you open the same notebook multiple times in different browser windows, be careful in making changes in different windows. Inconsistent changes may lead to conflicts or loss of your data.
-# 1. If your notebook fails to run any code, the Kernel might have died. You can restart the kernel with `Kernel->Restart`. If restarting fails, check your code cells to see if there are unsafe code that breaks the kernel.
+# 1. If your notebook fails to run any code, the Kernel might have died. You can restart the kernel with `Kernel->Restart`. If restarting fails, check your code cells to see if there is any code that breaks the kernel.
 
 # ### How to submit a notebook
 
-# - Although this lab does not count towards your final grade, you are required to submit it, to get familiar with the procedure.
-# - Before you submit, however, make sure everything runs as expected:
+# - Although Lab1 does not count towards your final grade, you are required to submit it, to get familiar with the procedure.
+# - Before you submit, make sure everything runs as expected:
 #     1. **Restart the kernel**: `Kernel->Restart` 
 #     1. **run all cells**: `Cell->Run All`
 
@@ -110,12 +95,23 @@ sys.stdout = old_stdout
 # 1. Go to `Assignment` tab of JupyterHub where you fetched the Lab assignment. 
 # 1. Expand the Lab1 folder and click the `validate` button next to the notebook(s) to check if all visible tests pass.
 # 1. Click `Submit` to submit your notebook. 
-# 1. You may submit as many times as you wish but we will only collect your latest submission before the [Lab 1 due date](https://canvas.cityu.edu.hk/courses/36768/assignments/128315?return_to=https%3A%2F%2Fcanvas.cityu.edu.hk%2Fcalendar%3Finclude_contexts%3Dcourse_36768%23view_name%3Dmonth%26view_start%3D2020-09-01) for grading.
+# 1. You may submit as many times as you wish before the due date as we collect your latest submission for grading. 
+#     - *No late submission* will be collected without valid justifications.
+#     - *Double check* that you have submitted the correct Lab assignment.
+#     - You are responsible for *recording your submission attempt* with a valid timestamp in case of technical issues.
 
 # **Tips**
 # 1. You normally have at least 5 days to work on the lab after your lab session. 
-# 1. You can check the due dates of all the labs from the [course homepage](https://canvas.cityu.edu.hk/courses/36768) or [course calendar](https://canvas.cityu.edu.hk/calendar?include_contexts=course_36768). 
-# 1. You may seek help from us or your classmates. However, you must write your own solution and indicate who your collaborators are using the code `COLLABORATORS = 'WONG Xiu Fong, LEE Man Kit'`.
+# 1. You can check the due dates of all the labs from the course homepage. 
+# 1. You may seek help from us or your classmates. However, you must write your own solution and indicate who your collaborators are using the code:
+
+# In[7]:
+
+
+COLLABORATORS = ['WONG Xiu Fong', 'LEE Man Kit']
+
+
+# ## Advanced Usage
 
 # ### How to print or backup a notebook?
 
@@ -132,3 +128,21 @@ sys.stdout = old_stdout
 # - remotely on other JupyHub services such as [Google Colab](https://colab.research.google.com/).
 # 
 # However, you would need to learn how to manage and install the additional packages required by the course.
+
+# ### Jupyter Lab and extensions
+
+# Instead of the classic notebook interface, you may also play with the new interface called JupyterLab by visiting [ltjh.cs.cityu.edu.hk/user-redirect/lab/](https://ltjh.cs.cityu.edu.hk/user-redirect/lab/). 
+# 
+# 
+# Note that the new interface does not support the validation and submission of lab assignment. It is currently under [active development on GitHub](https://github.com/jupyterlab/jupyterlab), so be prepared to see [bugs](https://en.wikipedia.org/wiki/Software_bug).
+
+# You may use the [visual debugger](https://github.com/jupyterlab/debugger) in JupyterLab to debug a jupyter notebook. To do you, you should open the notebook and choose XPython as the kernel.
+
+# Both the notebook/lab interface is extensible. For the notebook interface, you can enable extensions from the [nbextensions page](https://ltjh.cs.cityu.edu.hk/user-redirect/nbextensions).
+
+# ### Visual Studio Code
+
+# For a complete IDE experience, you can open VS Code as follows:
+# - [ltjh.cs.cityu.edu.hk/user-redirect/vscode/](https://ltjh.cs.cityu.edu.hk/user-redirect/vscode/)
+# - In classic notebook interface: `File` tab -> `New` menu -> `VS Code` menu item.
+# - In JupyterLab interface: `File` menu -> `New Launcher` menu item -> `VS Code` icon
